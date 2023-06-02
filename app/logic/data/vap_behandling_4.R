9c45c99e56693d015a5fa585781acacb6c0c394f
simonsteiger <simon.j.steiger@gmail.com>
2023-06-02 15:49:29 +0200

    Add data/vap_behandling_4

---
+++ app/logic/data/vap_behandling_4.R
@@ -0,0 +1,56 @@
+box::use(
+    fst,
+    magrittr[`%>%`],
+    dp = dplyr,
+    pr = purrr,
+    ts = tidyselect,
+)
+
+box::use(
+    ski = swissknife / skinit, # dl data from server in deployment
+    srqlib / srqdict, # include in project for deployment?
+    srqlib / srqprep, # include in project for deployment?
+)
+
+# here goes data base download later
+ski$read_dir("/Users/simonsteiger/Desktop/data/fst/")
+
+# the data needs to go through the qrdf preprocessing, too
+
+list_df$basdata <- list_df$basdata %>%
+    srqprep$prep_recode(diagnoskod_1, srqdict$rec_dxcat, .new_name = dxcat) %>%
+    dp$filter(dxcat == "RA") %>%
+    dp$select(patientkod, fodelsedag, dxcat, lan, tillhor)
+
+list_df$besoksdata <- list_df$besoksdata %>%
+    dp$select(patientkod, datum, das28, haq, patientens_globala) %>%
+    dp$mutate(patientens_globala = as.numeric(patientens_globala)) # unparsable strings become NA
+
+list_df$bio <- list_df$bio %>%
+    dp$arrange(patientkod, ordinerat) %>%
+    dp$distinct(patientkod, .keep_all = TRUE) %>%
+    dp$mutate(
+        prep_start = pmax(ordinerat, insatt, na.rm = TRUE),
+        preparat = ifelse(preparat == "Roactemra", "RoActemra", preparat)
+    )
+
+list_df$bas_bio <-
+    dp$inner_join(
+        list_df$bio, list_df$basdata,
+        by = "patientkod",
+        suffix = c("", ".dupl")
+    ) %>%
+    dp$select(-ts$contains(c(".dupl", "skapad", "andrad")), -c("preparat_kod", "orsak", "ar"))
+
+out <-
+    dp$left_join(list_df$bas_bio, list_df$besoksdata, by = "patientkod") %>%
+    dp$mutate(
+        diff = as.numeric(datum - prep_start),
+        visit_group = factor(dp$case_when(
+            diff >= -30 & diff <= 7 ~ "Behandlingsstart",
+            diff >= 120 & diff <= 365 ~ "Uppföljning",
+            .default = NA
+        ))
+    )
+
+fst$write_fst(out, "app/logic/data/vap_behandling_4.fst")
