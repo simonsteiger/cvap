98d2201eceb3b9db351c662b1230885efa78ba56
simonsteiger <simon.j.steiger@gmail.com>
2023-08-02 12:37:28 +0200

    Add tests for fil_ongoing and lookback

--- tests/testthat/test-lookback.R
+++ tests/testthat/test-lookback.R
@@ -4,26 +4,42 @@ box::use(
   magrittr[`%>%`],
   dp = dplyr,
   ts = tidyselect,
+  rl = rlang[`%||%`],
 )
 
 box::use(
   app / view / wrangle / lookback[lookback],
 )
 
-ref <- read.csv("app/logic/data/test/testdata_ongoing.csv", sep = ";") %>%
-  dp$mutate(dp$across(ts$where(is.character)), ~ as.factor(.x))
+ref <- read.csv("app/logic/data/test/ongoing.csv", sep = ";") %>%
+  dp$mutate(dp$across(ts$where(is.character), lub$as_date))
 
 tt$test_that("lookback errors if `.data` not reactive", {
   tt$expect_error(lookback(mtcars, list(lookback = 2)))
 })
 
+tt$test_that("lookback renames input$outcome to `outcome`", {
+  target <- "measure" # measure is the toy column with our outcome of interest
+  idx <- ref$measure[which(names(ref) == target)] # get index of that column
+  test <- lookback(ref, list(outcome = target, ongoing = lub$ymd("2020-01-01"), lookback = 3))
+  tt$expect_equal(names(test[idx]), "outcome")
+})
 
+tt$test_that("lookback renames .var to `outcome`", {
+  target <- "measure" # measure is the toy column with our outcome of interest
+  idx <- ref$measure[which(names(ref) == target)] # get index of that column
+  test <- lookback(ref, list(ongoing = lub$ymd("2020-01-01"), lookback = 3), .var = target)
+  tt$expect_equal(names(test[idx]), "outcome")
+})
 
-lookback <- function(.data, input, .var = NULL) {
-  stopifnot(!sh$is.reactive(.data)) # no reactive input
-  .data %>%
+tt$test_that("lookback filters correctly", {
+  input <- list(ongoing = lub$ymd("2020-01-01"), lookback = 3)
+  .var <- "measure"
+  # Steps below are relevant for correct filtering
+  # If fil_ongoing breaks, there will be an error in test-srqdict, too
+  # The remaining functions are dplyr
+  ref_sub <- ref %>%
     dp$rename(outcome = .data[[.var %||% input$outcome]]) %>%
-    # separately get rid of missing outcome values to avoid picking NA outcomes
     dp$filter(!is.na(outcome)) %>%
     dp$filter(
       !!srqdict$fil_ongoing(input$ongoing),
@@ -31,4 +47,6 @@ lookback <- function(.data, input, .var = NULL) {
     ) %>%
     dp$arrange(patientkod, dp$desc(datum)) %>%
     dp$distinct(patientkod, .keep_all = TRUE)
-}
+  test <- lookback(ref, input, .var)
+  tt$expect_equal(test, ref_sub)
+})
--- tests/testthat/test-srqdict.R
+++ tests/testthat/test-srqdict.R
@@ -1,7 +1,17 @@
 box::use(
+  dp = dplyr,
+  lub = lubridate,
+)
+
+box::use(
   app / logic / srqlib / srqdict
 )
 
-test_that("fil_ongoing works", {
-  expect_equal(2 * 2, 4)
+ref <- read.csv("app/logic/data/test/ongoing.csv", sep = ";") %>%
+  dp$mutate(dp$across(ts$where(is.character), lub$as_date))
+
+tt$test_that("fil_ongoing works as intended", {
+  at <- lub$ymd("2015-01-01")
+  ref_sub <- dp$filter(ref, ordinerat <= at & (pagaende == 1 | utsatt > at))
+  tt$expect_equal(dp$filter(ref, !!srqdict$fil_ongoing(at)), ref_sub)
 })
