a2d71ea1ff4a06ff5f20e3126da9891eef9267f7
simonsteiger <simon.j.steiger@gmail.com>
2023-10-03 15:55:38 +0200

    Fix y axis ticks outside view
    Added a left margin to echarts plot. This prevents y axis ticks being placed outside the plotting area.

--- app/logic/aux_server/output/bars.R
+++ app/logic/aux_server/output/bars.R
@@ -108,6 +108,8 @@ plot_bar_export <- function(.data, x, y, group, timeline, stash, input, format) 
     }
 }
 
+left_margin <- "17%" # 17% means no hidden text before responsive breakpoint
+
 plot_bar_interactive_core <- function(.data, input, x, y, timeline, text) {
     e <- .data %>%
         e4r$e_charts_(x, timeline = timeline) %>%
@@ -129,7 +131,8 @@ plot_bar_interactive_core <- function(.data, input, x, y, timeline, text) {
         ) %>%
         e4r$e_tooltip(textStyle = list(fontFamily = "Roboto")) %>%
         e4r$e_aria(enabled = TRUE, decal = list(show = TRUE)) %>% # decal patterns
-        e4r$e_theme_custom("app/static/echarts_theme.json")
+        e4r$e_theme_custom("app/static/echarts_theme.json") %>%
+        e4r$e_grid(left = left_margin) # left margin to avoid names being cut off
 
     if (timeline) {
         e4r$e_y_axis_(e, max = 1)
